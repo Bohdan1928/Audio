c49c1fb200f917919fc4f30cb7e7a1fa4e301c93
PC <bbigun2000@gmail.com>
2021-10-21 23:29:52 +0300

    Initial commit

---
+++ app/src/main/java/com/example/audio/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.audio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaParser;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+
+    public void resume(View view) {
+        mediaPlayer.start();
+    }
+}
