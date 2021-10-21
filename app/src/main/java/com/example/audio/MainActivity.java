ed148f1fb197dc6bfaf274e3ea590fc02daf1b8e
PC <bbigun2000@gmail.com>
2021-10-22 00:05:54 +0300

    one button

--- app/src/main/java/com/example/audio/MainActivity.java
+++ app/src/main/java/com/example/audio/MainActivity.java
@@ -6,24 +6,33 @@ import android.media.MediaParser;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    Button button;
+    private boolean flag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+        button = findViewById(R.id.button);
     }
 
-    public void pause(View view) {
-        mediaPlayer.pause();
-    }
+    public void change(View view) {
+        if (flag) {
+            mediaPlayer.pause();
+            button.setText("PLAY");
+        } else {
+            mediaPlayer.start();
+            button.setText("STOP");
 
-    public void resume(View view) {
-        mediaPlayer.start();
+        }
+        flag = !flag;
     }
 }
