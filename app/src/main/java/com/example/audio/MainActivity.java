bd517b586f412d08695745a9ba821ce1010b5b89
PC <bbigun2000@gmail.com>
2021-10-22 00:21:29 +0300

    SeekBar

--- app/src/main/java/com/example/audio/MainActivity.java
+++ app/src/main/java/com/example/audio/MainActivity.java
@@ -2,17 +2,21 @@ package com.example.audio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaParser;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mediaPlayer;
+    SeekBar seekBar;
     Button button;
+    AudioManager audioManager;
     private boolean flag = false;
 
     @Override
@@ -21,6 +25,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.stuff);
+
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(maxVolume);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
         button = findViewById(R.id.button);
     }
 
